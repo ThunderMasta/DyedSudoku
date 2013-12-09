39a6973fee16d88b8a4e2f8f5e078406e4a9870a
ThunderMasta <frol128@ya.ru>
2013-12-09 20:43:13 +0400

    Exit by tap

--- DyedSudoku/DyedSudoku/PlayLocalViewController.cs
+++ DyedSudoku/DyedSudoku/PlayLocalViewController.cs
@@ -7,90 +7,101 @@ using System.Threading.Tasks;
 
 namespace DyedSudoku
 {
-    public partial class PlayLocalViewController : UIViewController
-    {
-        // Need update thread for custom animation
-        private volatile bool needUpdateGameField;
-        private GameFieldViewModel gameFieldViewModel;
+	public partial class PlayLocalViewController : UIViewController
+	{
+		// Need update thread for custom animation
+		private volatile bool needUpdateGameField;
+		private GameFieldViewModel gameFieldViewModel;
 
-        public PlayLocalViewController() : base ("PlayLocalViewController", null)
-        {
-            InitGameFieldView();
+		public PlayLocalViewController() : base("PlayLocalViewController", null)
+		{
+			InitGameFieldView();
 
-            StartUpdateThread();
-        }
+			StartUpdateThread();
+		}
 
-        private void InitGameFieldView()
-        {
-            var width = View.Frame.Width - 10;
-            var frame = new RectangleF(5, gameFieldView.Frame.Y, width, width);
+		private void InitGameFieldView()
+		{
+			var width = View.Frame.Width - 10;
+			var frame = new RectangleF(5, gameFieldView.Frame.Y, width, width);
 
-            gameFieldView.Frame = frame;
-            gameFieldViewModel = new GameFieldViewModel(frame);
-            gameFieldView.SetDataSource(gameFieldViewModel);
-        }
+			gameFieldView.Frame = frame;
+			gameFieldViewModel = new GameFieldViewModel(frame);
+			gameFieldView.SetDataSource(gameFieldViewModel);
+		}
 
-        private void StartUpdateThread()
-        {
-            needUpdateGameField = true;
+		private void StartUpdateThread()
+		{
+			needUpdateGameField = true;
 			UpdateGameField();
-        }
+		}
 
-        private void StopUpdateThread()
-        {
-            needUpdateGameField = false;
-        }
+		private void StopUpdateThread()
+		{
+			needUpdateGameField = false;
+		}
 
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
 
-            // Perform any additional setup after loading the view, typically from a nib.
-        }
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
 
-        public override void DidReceiveMemoryWarning()
-        {
-            // Releases the view if it doesn't have a superview.
-            base.DidReceiveMemoryWarning();
+		public override void DidReceiveMemoryWarning()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning();
 			
-            // Release any cached data, images, etc that aren't in use.
-        }
+			// Release any cached data, images, etc that aren't in use.
+		}
 
-        public event EventHandler Done;
+		public event EventHandler Done;
 
-        partial void done(MonoTouch.Foundation.NSObject sender)
-        {
-            StopUpdateThread();
+		partial void done(MonoTouch.Foundation.NSObject sender)
+		{
+			FireDone();
+		}
 
-            if (gameFieldViewModel != null)
-                gameFieldViewModel.Cancel();
+		private void FireDone()
+		{
+			StopUpdateThread();
 
-            if (Done != null)
-                Done(this, EventArgs.Empty);
-        }
+			if (gameFieldViewModel != null)
+				gameFieldViewModel.Cancel();
 
-        private async void UpdateGameField()
-        {
-            while (needUpdateGameField)
-            {
-                //Sleep for 10 fps
-                await Task.Delay(100);
-                BeginInvokeOnMainThread(gameFieldView.SetNeedsDisplay);
-            }
-        }
+			if (Done != null)
+				Done(this, EventArgs.Empty);
+		}
 
-        partial void singleTap(NSObject sender)
-        {
-            var tapRecognizer = (UITapGestureRecognizer)sender;
+		private async void UpdateGameField()
+		{
+			while (needUpdateGameField)
+			{
+				//Sleep for 10 fps
+				await Task.Delay(100);
+				BeginInvokeOnMainThread(gameFieldView.SetNeedsDisplay);
+			}
+		}
 
-            var point = tapRecognizer.LocationInView(View);
-            var gameFieldPoint = tapRecognizer.LocationInView(gameFieldView);
+		partial void singleTap(NSObject sender)
+		{
+			if (gameFieldViewModel.IsEnd)
+			{
+				FireDone();
+				return;
+			}
 
-            if (gameFieldView.Frame.Contains(point))
-                gameFieldViewModel.UpdateByTap(gameFieldPoint);
-            else
-                gameFieldViewModel.UpdateByOffboardTap();
-        }
-    }
+			var tapRecognizer = (UITapGestureRecognizer)sender;
+
+			var point = tapRecognizer.LocationInView(View);
+			var gameFieldPoint = tapRecognizer.LocationInView(gameFieldView);
+
+			if (gameFieldView.Frame.Contains(point))
+				gameFieldViewModel.UpdateByTap(gameFieldPoint);
+			else
+				gameFieldViewModel.UpdateByOffboardTap();
+		}
+	}
 }
 
--- DyedSudoku/DyedSudoku/ViewModels/GameFieldViewModel.cs
+++ DyedSudoku/DyedSudoku/ViewModels/GameFieldViewModel.cs
@@ -5,340 +5,345 @@ using Common;
 
 namespace DyedSudoku
 {
-    public class GameFieldViewModel
-    {
-        private enum EMode
-        {
-            Normal,
-            Dialog,
-            Win,
-            Lose
-        }
+	public class GameFieldViewModel
+	{
+		private enum EMode
+		{
+			Normal,
+			Dialog,
+			Win,
+			Lose
+		}
 
-        private FPSCounter fpsCounter = new FPSCounter();
+		private readonly FPSCounter fpsCounter = new FPSCounter();
 
-        public RectangleF Frame { get; private set; }
+		public RectangleF Frame { get; private set; }
 
-        private IndexPair SelectedPair { get; set; }
+		private IndexPair SelectedPair { get; set; }
 
-        private GameFieldModel Model { get; set; }
+		private GameFieldModel Model { get; set; }
 
-        private DialogViewModel DialogViewModel { get; set; }
+		private DialogViewModel DialogViewModel { get; set; }
 
-        private EMode Mode { get; set; }
+		private EMode Mode { get; set; }
 
-        private float cellHeight;
-        private float cellWidth;
-        private float blockHeight;
-        private float blockWidth;
-        private CGContext context;
-        private const float cellContentLeft = 10;
-        private const float cellContentBottom = 8;
-        private const float dialogResultWidth = 250;
-        private const float dialogResultHeight = 150;
-        private const float dialogBorder = 3;
-        private const float dialogContentBottom = 30;
-        private const float dialogOffset = 1;
+		public bool IsEnd
+		{
+			get { return Mode == EMode.Win || Mode == EMode.Lose; }
+		}
 
-        public GameFieldViewModel(RectangleF frame)
-        {
-            InitModel();
-            InitCellSizes(frame);
-        }
+		private float cellHeight;
+		private float cellWidth;
+		private float blockHeight;
+		private float blockWidth;
+		private CGContext context;
+		private const float cellContentLeft = 10;
+		private const float cellContentBottom = 8;
+		private const float dialogResultWidth = 250;
+		private const float dialogResultHeight = 150;
+		private const float dialogBorder = 3;
+		private const float dialogContentBottom = 30;
+		private const float dialogOffset = 1;
 
-        public void InitModel()
-        {
-            Model = new GameFieldModel();
-            Mode = EMode.Normal;
-        }
+		public GameFieldViewModel(RectangleF frame)
+		{
+			InitModel();
+			InitCellSizes(frame);
+		}
 
-        public void Cancel()
-        {
-            Model.Cancel();
-        }
+		public void InitModel()
+		{
+			Model = new GameFieldModel();
+			Mode = EMode.Normal;
+		}
 
-        public void InitCellSizes(RectangleF frame)
-        {
-            Frame = frame;
+		public void Cancel()
+		{
+			Model.Cancel();
+		}
 
-            cellHeight = Frame.Height / Model.CellLineCount;
-            cellWidth = Frame.Width / Model.CellLineCount;
-            blockHeight = Frame.Height / Model.BlockLineCount;
-            blockWidth = Frame.Width / Model.BlockLineCount;
-        }
+		public void InitCellSizes(RectangleF frame)
+		{
+			Frame = frame;
 
-        public void Draw(CGContext ctx)
-        {            
-            fpsCounter.Tick();
+			cellHeight = Frame.Height / Model.CellLineCount;
+			cellWidth = Frame.Width / Model.CellLineCount;
+			blockHeight = Frame.Height / Model.BlockLineCount;
+			blockWidth = Frame.Width / Model.BlockLineCount;
+		}
 
-            SetCurrentContext(ctx);
+		public void Draw(CGContext ctx)
+		{            
+			fpsCounter.Tick();
 
-            if (Model.IsInitializing)
-            {
-                DrawBackground();
-                DrawCellsBackground();
-            }
-            else
-            {
-                DrawSelected();
-                DrawModel();
-            }
+			SetCurrentContext(ctx);
 
-            DrawLines();
-            DrawDialogs();
+			if (Model.IsInitializing)
+			{
+				DrawBackground();
+				DrawCellsBackground();
+			}
+			else
+			{
+				DrawSelected();
+				DrawModel();
+			}
+
+			DrawLines();
+			DrawDialogs();
 			//DrawFPS();
-        }
+		}
 
-        private void SetCurrentContext(CGContext ctx)
-        {
-            context = ctx;
+		private void SetCurrentContext(CGContext ctx)
+		{
+			context = ctx;
 
-            context.SetDefaultCTMSettings(Frame.Height);
-            context.SetDefaultTextSettings();
-        }
+			context.SetDefaultCTMSettings(Frame.Height);
+			context.SetDefaultTextSettings();
+		}
 
-        private void DrawBackground()
-        {
-            context.SetFillEmptyItemColor();
-            context.AddRect(new RectangleF(0, 0, Frame.Width, Frame.Height));
-            context.DrawPath(CGPathDrawingMode.Fill);
-        }
+		private void DrawBackground()
+		{
+			context.SetFillEmptyItemColor();
+			context.AddRect(new RectangleF(0, 0, Frame.Width, Frame.Height));
+			context.DrawPath(CGPathDrawingMode.Fill);
+		}
 
-        private void DrawCellsBackground()
-        {
-            foreach (var pair in Model.GetAllPairs())
-            {
-                if (Model.IsItemEmpty(pair))
-                    continue;
+		private void DrawCellsBackground()
+		{
+			foreach (var pair in Model.GetAllPairs())
+			{
+				if (Model.IsItemEmpty(pair))
+					continue;
 
 				if (Model.IsSelectedPair(pair))
-				    context.SetFillSelectedItemColor();
+					context.SetFillSelectedItemColor();
 				else if (Model.GetItemVisible(pair))
-                    context.SetFillVisibleItemColor();
-                else
-                    context.SetFillInitializedItemColor();
+					context.SetFillVisibleItemColor();
+				else
+					context.SetFillInitializedItemColor();
 
-                FillCellBackground(pair);
-            }
-        }
+				FillCellBackground(pair);
+			}
+		}
 
-        private void FillCellBackground(IndexPair pair)
-        {
-            context.AddRect(new RectangleF(pair.X * cellWidth, pair.Y * cellHeight, cellWidth, cellHeight));
-            context.DrawPath(CGPathDrawingMode.Fill);
-        }
+		private void FillCellBackground(IndexPair pair)
+		{
+			context.AddRect(new RectangleF(pair.X * cellWidth, pair.Y * cellHeight, cellWidth, cellHeight));
+			context.DrawPath(CGPathDrawingMode.Fill);
+		}
 
-        private void DrawLines()
-        {
-            context.SetDefaultLineSettings();
+		private void DrawLines()
+		{
+			context.SetDefaultLineSettings();
 
-            DrawCellLines();
-            DrawBlockLines();
-        }
+			DrawCellLines();
+			DrawBlockLines();
+		}
 
-        private void DrawCellLines()
-        {
-            DrawGrid(cellHeight, cellWidth, 1);
-        }
+		private void DrawCellLines()
+		{
+			DrawGrid(cellHeight, cellWidth, 1);
+		}
 
-        private void DrawBlockLines()
-        {
-            DrawGrid(blockHeight, blockWidth, 2);
-        }
+		private void DrawBlockLines()
+		{
+			DrawGrid(blockHeight, blockWidth, 2);
+		}
 
-        private void DrawGrid(float height, float width, float lineWidth)
-        {
-            context.SetLineWidth(lineWidth);
+		private void DrawGrid(float height, float width, float lineWidth)
+		{
+			context.SetLineWidth(lineWidth);
 
-            for (float i = 0; i <= Frame.Height; i += height)
-                context.AddLines(new[] { new PointF(0, i), new PointF(Frame.Width, i) });
+			for (float i = 0; i <= Frame.Height; i += height)
+				context.AddLines(new[] { new PointF(0, i), new PointF(Frame.Width, i) });
 
-            for (float i = 0; i <= Frame.Width; i += width)
-                context.AddLines(new[] { new PointF(i, 0), new PointF(i, Frame.Height) });
+			for (float i = 0; i <= Frame.Width; i += width)
+				context.AddLines(new[] { new PointF(i, 0), new PointF(i, Frame.Height) });
 
-            context.DrawPath(CGPathDrawingMode.Stroke);
-        }
+			context.DrawPath(CGPathDrawingMode.Stroke);
+		}
 
-        private void DrawModel()
-        {
-            context.SetDefaultTextSettings();
+		private void DrawModel()
+		{
+			context.SetDefaultTextSettings();
 
-            foreach (var pair in Model.GetAllPairs())
-            {
-                if (!Model.GetItemVisible(pair))
-                    continue;
+			foreach (var pair in Model.GetAllPairs())
+			{
+				if (!Model.GetItemVisible(pair))
+					continue;
 
-                context.DrawText(Model.GetItemNumber(pair).ToString(), pair.X * cellWidth + cellContentLeft, pair.Y * cellHeight + cellContentBottom);
-            }
-        }
+				context.DrawText(Model.GetItemNumber(pair).ToString(), pair.X * cellWidth + cellContentLeft, pair.Y * cellHeight + cellContentBottom);
+			}
+		}
 
-        private void DrawFPS()
-        {
-            context.SetDefaultInfoTextSettings();
+		private void DrawFPS()
+		{
+			context.SetDefaultInfoTextSettings();
 
-            context.DrawInfoText(DateTime.Now.ToLongTimeString(), Frame.Width - 100, Frame.Height - 20);
-            context.DrawInfoText(fpsCounter.GetFPS().ToString(), Frame.Width - 30, Frame.Height - 20);
-        }
+			context.DrawInfoText(DateTime.Now.ToLongTimeString(), Frame.Width - 100, Frame.Height - 20);
+			context.DrawInfoText(fpsCounter.GetFPS().ToString(), Frame.Width - 30, Frame.Height - 20);
+		}
 
-        private void DrawSelected()
-        {
-            if (SelectedPair == null)
-                return;
+		private void DrawSelected()
+		{
+			if (SelectedPair == null)
+				return;
 
-            context.SetFillSelectedItemColor();
-            FillCellBackground(SelectedPair);
-        }
+			context.SetFillSelectedItemColor();
+			FillCellBackground(SelectedPair);
+		}
 
-        public void UpdateByTap(PointF point)
-        {
-            if (Model.IsInitializing)
-                return;
+		public void UpdateByTap(PointF point)
+		{
+			if (Model.IsInitializing)
+				return;
 
-            var ctmPoint = new PointF(point.X, Frame.Height - point.Y);
+			var ctmPoint = new PointF(point.X, Frame.Height - point.Y);
 
-            if (Mode == EMode.Normal)
-            {
-                var x = (int)(ctmPoint.X / cellWidth);
-                var y = (int)(ctmPoint.Y / cellHeight);
-                var pair = new IndexPair(x, y);
+			if (Mode == EMode.Normal)
+			{
+				var x = (int)(ctmPoint.X / cellWidth);
+				var y = (int)(ctmPoint.Y / cellHeight);
+				var pair = new IndexPair(x, y);
 
-                if (Model.GetItemVisible(pair))
-                    return;
+				if (Model.GetItemVisible(pair))
+					return;
 
-                SelectedPair = pair;
+				SelectedPair = pair;
 
-                DialogViewModel = new DialogViewModel(GetDialogFrame(ctmPoint), dialogBorder);
+				DialogViewModel = new DialogViewModel(GetDialogFrame(ctmPoint), dialogBorder);
 
-                Mode = EMode.Dialog;
+				Mode = EMode.Dialog;
 
-                return;
-            }
+				return;
+			}
 
-            if (Mode == EMode.Dialog)
-            {
+			if (Mode == EMode.Dialog)
+			{
 				if (DialogViewModel == null || DialogViewModel.IsInvisible)
-                    return;
+					return;
 
-                if (DialogViewModel.IsInside(ctmPoint))
-                {
-                    Model.SetItemVisible(SelectedPair, true);
+				if (DialogViewModel.IsInside(ctmPoint))
+				{
+					Model.SetItemVisible(SelectedPair, true);
 
-                    if (DialogViewModel.GetNumber(ctmPoint) != Model.GetItemNumber(SelectedPair))
-                    {
-                        Mode = EMode.Lose;
-                    }
-                    else if (Model.IsAllItemsVisible)
-                    {
-                        Mode = EMode.Win;
-                    }
-                    else
-                    {
-                        Mode = EMode.Normal;
-                    }
-                }
-                else
-                {
-                    Mode = EMode.Normal;
-                }
+					if (DialogViewModel.GetNumber(ctmPoint) != Model.GetItemNumber(SelectedPair))
+					{
+						Mode = EMode.Lose;
+					}
+					else if (Model.IsAllItemsVisible)
+					{
+						Mode = EMode.Win;
+					}
+					else
+					{
+						Mode = EMode.Normal;
+					}
+				}
+				else
+				{
+					Mode = EMode.Normal;
+				}
 
-                SelectedPair = null;
-                DialogViewModel = null;
+				SelectedPair = null;
+				DialogViewModel = null;
 
-                return;
-            }
-        }
+				return;
+			}
+		}
 
-        public void UpdateByOffboardTap()
-        {
-            if (Mode != EMode.Dialog)
-                return;
+		public void UpdateByOffboardTap()
+		{
+			if (Mode != EMode.Dialog)
+				return;
 
-            Mode = EMode.Normal;
-            SelectedPair = null;
-            DialogViewModel = null;
-        }
+			Mode = EMode.Normal;
+			SelectedPair = null;
+			DialogViewModel = null;
+		}
 
-        private RectangleF GetDialogFrame(PointF point)
-        {
-            var dialogWidth = 4 * cellWidth;
-            var dialogHeight = 4 * cellHeight;
+		private RectangleF GetDialogFrame(PointF point)
+		{
+			var dialogWidth = 4 * cellWidth;
+			var dialogHeight = 4 * cellHeight;
 
-            var left = point.X - dialogWidth / 2;
-            var bottom = point.Y - dialogHeight / 2;
+			var left = point.X - dialogWidth / 2;
+			var bottom = point.Y - dialogHeight / 2;
 
-            if (left < dialogOffset)
-                left = dialogOffset;
+			if (left < dialogOffset)
+				left = dialogOffset;
 
-            if (bottom < dialogOffset)
-                bottom = dialogOffset;
+			if (bottom < dialogOffset)
+				bottom = dialogOffset;
 
-            if (left + dialogWidth > Frame.Width - dialogOffset)
-                left = Frame.Width - dialogWidth - dialogOffset;
+			if (left + dialogWidth > Frame.Width - dialogOffset)
+				left = Frame.Width - dialogWidth - dialogOffset;
 
-            if (bottom + dialogHeight > Frame.Height - dialogOffset)
-                bottom = Frame.Height - dialogHeight - dialogOffset;
+			if (bottom + dialogHeight > Frame.Height - dialogOffset)
+				bottom = Frame.Height - dialogHeight - dialogOffset;
 
-            return new RectangleF(left, bottom, dialogWidth, dialogHeight);
-        }
+			return new RectangleF(left, bottom, dialogWidth, dialogHeight);
+		}
 
-        public void DrawDialogs()
-        {
-            switch (Mode)
-            {
-                case EMode.Normal:
-                    break;
-                case EMode.Dialog:
-                    DrawDialog();
-                    break;
-                case EMode.Win:
-                case EMode.Lose:
-                    DrawResultInfo();
-                    break;
-                default:
-                    throw new ArgumentException("Mode");
-            }
-        }
+		public void DrawDialogs()
+		{
+			switch (Mode)
+			{
+				case EMode.Normal:
+					break;
+				case EMode.Dialog:
+					DrawDialog();
+					break;
+				case EMode.Win:
+				case EMode.Lose:
+					DrawResultInfo();
+					break;
+				default:
+					throw new ArgumentException("Mode");
+			}
+		}
 
-        public void DrawDialog()
-        {
-            if (DialogViewModel == null)
-                return;
+		public void DrawDialog()
+		{
+			if (DialogViewModel == null)
+				return;
 
-            DialogViewModel.Draw(context);
-        }
+			DialogViewModel.Draw(context);
+		}
 
-        public void DrawResultInfo()
-        {
-            var centerHeight = Frame.Height / 2;
-            var centerWigth = Frame.Width / 2;
-            var left = centerWigth - dialogResultWidth / 2;
-            var bottom = centerHeight - dialogResultHeight / 2;
+		public void DrawResultInfo()
+		{
+			var centerHeight = Frame.Height / 2;
+			var centerWigth = Frame.Width / 2;
+			var left = centerWigth - dialogResultWidth / 2;
+			var bottom = centerHeight - dialogResultHeight / 2;
 
-            context.SetFillDialogBorderColor();
+			context.SetFillDialogBorderColor();
 
-            context.AddRect(
-                new RectangleF(left, 
-                               bottom, 
-                               dialogResultWidth, 
-                               dialogResultHeight));
+			context.AddRect(
+				new RectangleF(left, 
+					bottom, 
+					dialogResultWidth, 
+					dialogResultHeight));
 
-            context.DrawPath(CGPathDrawingMode.Fill);
+			context.DrawPath(CGPathDrawingMode.Fill);
 
-            if (Mode == EMode.Win)
-                context.SetFillWinInfoBackgroundColor();
-            else
-                context.SetFillLoseInfoBackgroundColor();
+			if (Mode == EMode.Win)
+				context.SetFillWinInfoBackgroundColor();
+			else
+				context.SetFillLoseInfoBackgroundColor();
 
-            context.AddRect(
-                new RectangleF(left + dialogBorder, 
-                               bottom + dialogBorder, 
-                               dialogResultWidth - 2 * dialogBorder, 
-                               dialogResultHeight - 2 * dialogBorder));
+			context.AddRect(
+				new RectangleF(left + dialogBorder, 
+					bottom + dialogBorder, 
+					dialogResultWidth - 2 * dialogBorder, 
+					dialogResultHeight - 2 * dialogBorder));
 
-            context.DrawPath(CGPathDrawingMode.Fill);
+			context.DrawPath(CGPathDrawingMode.Fill);
 
-            context.SetDefaultTextSettings();
-            context.DrawResultText(Mode == EMode.Win ? "Win" : "Lose", centerWigth, centerHeight - dialogContentBottom, true);
-        }
-    }
+			context.SetDefaultTextSettings();
+			context.DrawResultText(Mode == EMode.Win ? "Win" : "Lose", centerWigth, centerHeight - dialogContentBottom, true);
+		}
+	}
 }
